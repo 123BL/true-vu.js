60c1718bc648c83f85b5e8ef5fc87d14ff31b6c5
Brandon Lu <123blu@frhsd.com>
2023-02-13 10:59:25 -0500

    how to use some stuff with vue.js (18:00 video 2)

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    // data, functions 
+    // template: '<h2>I am the template</h2>'
+    data () {
+        return {
+            title: 'idk',
+            author: 'Brandon Lu',
+            age: 20
+        }
+    },
+    methods: {
+        
+    }
+});
+
+app.mount('#app');
