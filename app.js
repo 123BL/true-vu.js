a15b8e9d6bd9a302195b5b0e98c1f23a827b42e5
Brandon Lu <123blu@frhsd.com>
2023-02-15 08:27:28 -0500

    we learned directives

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const app = Vue.createApp({
     // template: '<h2>I am the template</h2>'
     data () {
         return {
+            showBooks: true,
             title: 'idk',
             author: 'Brandon Lu',
             age: 20
         }
     },
     methods: {
-        
+toggleShowBooks() {
+this.showBooks = !this.showBooks
     }
+}
 });
+    // methods: {
+    //     changeTitle(title) {
+    //         // this.title = 'words of Randiances'
+    //         this.title = title
+    //     }
+    // }
+
 
 app.mount('#app');
