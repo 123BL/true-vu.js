bfaa52471a4cfd4c1e3509101220f1dcb1a340e3
123BL <71268010+123BL@users.noreply.github.com>
2023-02-16 17:37:46 -0500

    Add files via upload
    in order to insert a image with vue.js you'll need to bind the source in the html and then insert the images into app.js

---
+++ true-vu.js-master/true-vu.js-master/app.js
@@ -0,0 +1,46 @@
+const app = Vue.createApp({
+    // data, functions 
+    // template: '<h2>I am the template</h2>'
+    data () {
+        return {
+            url: 'http://www.thenetninja.co.uk',
+            showBooks: true,
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg'},
+                {title: 'the way of kind', author: 'Brandon Lu', img: 'assets/2.jpg'},
+                {title: 'the final empire', author: 'Brandon Sanderson', img: 'assets/3.jpg'},
+            ]
+            // thing used to name the showBooks
+
+            // title: 'idk',
+            // author: 'Brandon Lu',
+            // age: 20,
+            // x: 0,
+            // y: 0 
+        }
+    },
+    methods: {
+toggleShowBooks() {
+this.showBooks = !this.showBooks
+    },
+    handleEvent(e, data ) {
+        console.log(e, e.type)
+        if (data) {
+            console.log(data)
+        } 
+    },
+    handleMousemove(e) {
+this.x = e.offsetX
+this.y = e.offsetY
+    }
+}
+});
+    // methods: {
+    //     changeTitle(title) {
+    //         // this.title = 'words of Randiances'
+    //         this.title = title
+    //     }
+    // }
+
+
+app.mount('#app');
