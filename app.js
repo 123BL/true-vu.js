ae5b7d7dc06ed73df7f556ad35286bb04e9b29a0
Brandon Lu <123blu@frhsd.com>
2023-02-16 09:47:52 -0500

    worked on bind and mouse method (continue from video 3 part 2 18:28)

--- app.js
+++ app.js
@@ -3,15 +3,35 @@ const app = Vue.createApp({
     // template: '<h2>I am the template</h2>'
     data () {
         return {
+            url: 'http://www.thenetninja.co.uk',
             showBooks: true,
-            title: 'idk',
-            author: 'Brandon Lu',
-            age: 20
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss'},
+                {title: 'the way of kind', author: 'Brandon Lu'},
+                {title: 'the final empire', author: 'Brandon Sanderson'},
+            ]
+            // thing used to name the showBooks
+
+            // title: 'idk',
+            // author: 'Brandon Lu',
+            // age: 20,
+            // x: 0,
+            // y: 0 
         }
     },
     methods: {
 toggleShowBooks() {
 this.showBooks = !this.showBooks
+    },
+    handleEvent(e, data ) {
+        console.log(e, e.type)
+        if (data) {
+            console.log(data)
+        } 
+    },
+    handleMousemove(e) {
+this.x = e.offsetX
+this.y = e.offsetY
     }
 }
 });
